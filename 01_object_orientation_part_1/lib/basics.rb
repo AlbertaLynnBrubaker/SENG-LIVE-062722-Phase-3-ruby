c36d3dfa7ce94c36cfc108c5f4fa770f4ba59307
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-08 14:48:39 -0500

    End of lecture 20220808

---
+++ 01_object_orientation_part_1/lib/basics.rb
@@ -0,0 +1,20 @@
+require "pry"
+# Ruby Data Types
+# String
+# Number - Int, Float
+# Booleans
+# Array []
+# Symbol :key
+# Hash {}
+
+# Variable
+# name = "rose"
+# interpolation with double quotes -> "#{}"
+# puts "my cat #{name} is cute!"
+
+myHash = {cat: "Rose"}
+
+# JavaScript myHash.cat
+# Ruby myHash[:cat]
+
+binding.pry
--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,13 +1,39 @@
 class Dog
+  attr_accessor :name, :age, :breed, :image_url, :last_fed_at, :last_walked
   # ✅ we should be able to create dogs with a name, age, breed, and image_url
-
+  def initialize name, age, breed, image_url, last_fed_at, last_walked
+    @name = name
+    @age = age
+    @breed = breed
+    @image_url = image_url
+    @last_fed_at = last_fed_at
+    @last_walked = last_walked
+  end
   
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
+  # def name
+  #   @name
+  # end
 
+  # def age
+  #   @age
+  # end
 
+  # def age= new_age
+  #   @age = new_age
+  # end
+
+  
 
   # ✅ we want to be able to print details about a dog (this time including the last walked at and last fed at times)
-
+  def print
+    puts "-------------------------"
+    puts @name
+    puts @age
+    puts @breed
+    puts @image_url
+    puts "-------------------------"
+  end
 
 end
